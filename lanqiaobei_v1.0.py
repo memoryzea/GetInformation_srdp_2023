b09fd9835d048a21f306317de640cfe3cf5c346b
memoryzea <91736892+memoryzea@users.noreply.github.com>
2023-10-17 19:29:16 +0800

    Update lanqiaobei_v1.0.py（无筛选）

--- lanqiaobei_v1.0.py
+++ lanqiaobei_v1.0.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import pdfminer
+
 if __name__ == "__main__":
      headers = {
          'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60'
@@ -16,7 +18,7 @@ if __name__ == "__main__":
 
      id_list = []#储存比赛信息id
      text_list = []#存比赛信息文件
-     text_detail = []#储存比赛信息文件内容
+    #  text_detail #储存比赛信息文件内容
      json_ids = requests.get(url=url,headers=headers,params=data).json()
      for dic in json_ids['datalist']:
          id_list.append(dic['nnid'])
@@ -33,10 +35,21 @@ if __name__ == "__main__":
         for i in news_list[4]:  #将list中第五项content取出
             text_list.append(i)  
         
-        print(text_list[1])   #第五项元组的第二项取出（去除content：）
+        text_detail = "".join(text_list[1])  #第五项元组的第二项取出（去除content：）并转化为Str类型存在text_detail中
+        # print(text_detail)
 
-        #目前是如何取出content中的text内容
-        
+        # pattern = re.compile(r'[\u4e00-\u9fa5]+')
+        # text_result = pattern.findall(text_detail)
+        # print(text_result)
+
+     with open('关于开展第十五届蓝桥杯大赛“全国高校备赛季”活动的通知.pdf', 'w',encoding='utf-8') as f:
+         f.write(text_detail)
+     with open('关于开展第十五届蓝桥杯大赛“全国高校备赛季”活动的通知.pdf', 'rb') as file:
+         soup = BeautifulSoup(file, 'html.parser')
+     text = soup.get_text()
+     with open('output.txt', 'w',encoding = 'utf-8') as f:
+         f.write(text)
+
 
             
         
