49b0c4bd8ecbb2c976b127b1e42d33d131213db6
Baidy123 <142412118+Baidy123@users.noreply.github.com>
2023-10-26 22:57:47 +0800

    v0.2 美赛爬虫

---
+++ mcm.py
@@ -0,0 +1,69 @@
+import html2text
+import requests
+from bs4 import BeautifulSoup
+import redis
+# import re 
+
+conn = redis.Redis()    #比较是否爬取的东西
+# sample = "报名"   #输入识别相关内容
+class cet():
+    def __init__(self):
+        self.url = 'https://www.contest.comap.com/undergraduate/contests/mcm/instructions.php'
+        self.header = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60'
+        }
+
+        
+    def crawl(self,url):
+        res = requests.get(url, headers = self.header)
+        if res.status_code == 200:
+            res.encoding = 'utf=8'
+            return res
+
+    
+    def spider(self):
+        print('开始爬取目标网页...')
+        res = self.crawl(self.url)
+        soup = BeautifulSoup(res.text,'lxml')
+        title = soup.find('span', style='font-size:16.0pt;mso-bidi-font-size:12.0pt')
+        # print(title.text)
+        flg = conn.sadd('5爱是caaxzxaaa',title.text) # 随机字符串，勿与之前相同
+        if flg:
+            print('MCM is downloading...')
+            content_element = soup.find('div', class_='Section1')
+            content = self.html_to_text(str(content_element))
+            self.save(content)
+        else:
+            #已爬取，等待24h再次爬取
+            import time
+            print('暂无新信息,将于24小时后再次爬取...')
+            time.sleep(60*60*24)
+            self.spider()
+        print('爬取完毕')
+        print('*************************************************************************************')
+            
+    
+    def html_to_text(self, html_content):
+        h = html2text.HTML2Text()
+        h.ignore_links = True  # 设置为True以忽略链接
+        h.ignore_images = True  # 设置为True以忽略图像
+        # 将 HTML 转换为 Markdown 格式的文本
+        markdown_text = h.handle(html_content)
+        return markdown_text
+    
+    def save(self,content):
+        fp = open('MCM通知.txt','w',encoding='utf-8')
+        fp.write(content) #写入文件
+        print("MCM通知.txt has been loaded...")
+    
+    def run(self):
+        print('爬虫程序开始运行...')
+        while True:
+            self.spider()
+    
+if __name__ == '__main__':
+    x = cet()
+    x.run()
+    
+    
+    
