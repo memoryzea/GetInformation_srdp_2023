5b81d955bb8c0eda0c594422de567dc6a17d0d69
Baidy123 <142412118+Baidy123@users.noreply.github.com>
2023-10-26 23:11:36 +0800

    v0.21 优化（省去）循环

--- nuedc.py
+++ nuedc.py
@@ -5,7 +5,7 @@ import re
 import html2text
 
 conn = redis.Redis()    #比较是否爬取的东西
-sample = "关于召开 2023 年全国大学生电子设计竞赛总结暨颁奖大会的通知"   #输入识别相关内容
+sample = ""   #输入识别相关内容
 class nuedc():
     def __init__(self):
         self.url = 'https://www.nuedc-training.com.cn/index/news/index/id/1'
@@ -25,30 +25,29 @@ class nuedc():
         print('开始爬取目标网页...')
         res = self.crawl(self.url)
         soup = BeautifulSoup(res.text,'lxml')
-        for list in soup.select('.new-main .newsMain-liText>a'):
-            try:
-                if re.search(sample,list.text):
-                    title = list.text
-                    # print(title)
-                    flg = conn.sadd('1e1asaaas',title) # 随机字符串，勿与之前相同
-                    if flg:
-                        print(title+" is loading...")
-                        detail_url = list['href']
-                        detail_txt = self.crawl(detail_url).text
-                        detail_soup = BeautifulSoup(detail_txt,'lxml')
-                        content_element = detail_soup.find('div', class_='newsMain-content')
-                        content = self.html_to_text(str(content_element))
-                        self.save(content, title)
-                    else:
+        list = soup.find('div', class_='newsMain-liText')
+        if re.search(sample,list.text):
+            title = list.a.text
+            # print(title)
+            flg = conn.sadd('1e啊啊axaas',title) # 随机字符串，勿与之前相同
+            if flg:
+                print(title+" is loading...")
+                detail_url = list.a['href']
+                detail_txt = self.crawl(detail_url).text
+                detail_soup = BeautifulSoup(detail_txt,'lxml')
+                content_element = detail_soup.find('div', class_='newsMain-content')
+                content = self.html_to_text(str(content_element))
+                self.save(content, title)
+            else:
                         import time
                         print('暂无新信息,将于24小时后再次爬取...')
                         time.sleep(60*60*24)
                         self.spider()
-                else: 
-                    pass    
-            except KeyError:
-                print("KeyError occurs...")
-        # print('爬取完毕...')
+        else: 
+            pass    
+
+        print('爬取完毕...')
+        print('**************************************************************************')
     
     def html_to_text(self, html_content):
         h = html2text.HTML2Text()
@@ -62,13 +61,13 @@ class nuedc():
     
     def save(self,content,title):
         fp = open(title+'.txt','w',encoding='utf-8')
-        fp.write('                                                                '+title+"\n"+content+"\n") #写入文件
-        print(title+" has been loaded...\n-------------------------------------------------------------------------------\n")
+        fp.write(content+"\n") #写入文件
+        print(title+" has been loaded...")
     
     def run(self):
         print('爬虫程序开始运行...')
-        while True:
-            self.spider()
+        # while True:
+        self.spider()
     
 if __name__ == '__main__':
     x = nuedc()
