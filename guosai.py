126bfc08f0c5923a607b021f0168dd1889f0ea42
Baidy123 <142412118+Baidy123@users.noreply.github.com>
2023-10-27 21:57:52 +0800

    Update guosai.py

--- guosai.py
+++ guosai.py
@@ -5,10 +5,10 @@ import re
 import PyPDF2
 import pymysql
 
-conn = redis.Redis()    #比较是否爬取的东西
-sample = "2023高教社杯全国大学生数学建模竞赛报名"   #输入识别相关内容
-class guosai():
+
+class class_guosai():
     def __init__(self):
+        self.sample_guosai = "2023高教社杯全国大学生数学建模竞赛报名"   #输入识别相关内容
         self.url = 'http://www.mcm.edu.cn/html_cn/block/20ead73cbcf5a1c24b91947f98d7aac2.html'
         self.header = {
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60'
@@ -28,9 +28,9 @@ class guosai():
         soup = BeautifulSoup(res.text,'lxml')
         for list in soup.select('.item>a'):
             try:
-                if re.search(sample,list.text):
+                if re.search(self.sample_guosai,list.text):
                     title = list.text
-                    flg = conn.sadd('166s15',title) # 随机字符串，勿与之前相同
+                    flg = conn.sadd('1asa15',title) # 随机字符串，勿与之前相同
                     if flg:
                         print(title+" is loading...")
                         detail_url = 'http://www.mcm.edu.cn'+list['href']
@@ -43,7 +43,7 @@ class guosai():
                     else:
                         import time
                         print('暂无新信息,将于24小时后再次爬取...')
-                        time.sleep(60*60*24)
+                        time.sleep(60*2)
                         self.spider()
                 else: 
                     pass
@@ -65,8 +65,8 @@ class guosai():
         with open(title+'.txt', 'w',encoding='utf-8') as txt_file:
             txt_file.write(text)
         pdf_file.close()
-        self.todatabase(content=text,title=title)
-        print(title+" has been loaded...\n-------------------------------------------------------------------------------\n")
+        # self.todatabase(content=text,title=title)
+        print(title+" has been loaded...")
     
     def todatabase(self,title,content):
         conn = pymysql.connect(host='127.0.0.1',port=3306,user='tester',password='Srdp20232',db = 'comp_srdp')
@@ -84,5 +84,6 @@ class guosai():
             self.spider()
     
 if __name__ == '__main__':
-    x = guosai()
+    conn = redis.Redis()    #比较是否爬取的东西
+    x = class_guosai()
     x.run()
